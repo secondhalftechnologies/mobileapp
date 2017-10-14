60b8776625b688d17c2cfcadde001350e6138239
Ejaz Ansari (Ejaz47) <ejjuak47@gmail.com>
2017-10-14 07:23:35 -0700

    Knowledge and phone point calc

--- src/pages/kyc-knowledge/kyc-knowledge.ts
+++ src/pages/kyc-knowledge/kyc-knowledge.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 /**
@@ -16,9 +16,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class KycKnowledgePage {
 
 	knowledge: FormGroup;
+	submitAttempt: boolean = false;
 	constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, private loadingCtrl: LoadingController) {
+		//creating form via formbuilder 
 		this.knowledge = formBuilder.group({
-			'f2_points' : [''],
+			'f2_points' : ['0'],
 
 			'f2_edudetail' : ['', Validators.required],
 			'f2_proficiency' : ['', Validators.required],
@@ -31,21 +33,117 @@ export class KycKnowledgePage {
 	}
 
 	ionViewDidLoad() {
-		let loading = this.loadingCtrl.create({
-		    content: 'Loading data...'
+		// let loading = this.loadingCtrl.create({
+		//     content: 'Loading data...'
+		// });
+		// loading.present();
+		// console.log('ionViewDidLoad KycKnowledgePage', this.knowledge);
+		// setTimeout(() => {
+		//     loading.dismiss();
+		// }, 1000);
+		
+		//update validation here
+		this.setValidation();
+
+		//Listen for form changes
+		this.knowledge.controls['f2_edudetail'].valueChanges.subscribe(() => {
+			this.knowledge.get('f2_points').setValue(this.getTotal(), { emitEvent: false });
+			console.log(this.knowledge.get('f2_points').value);
 		});
-		loading.present();
-		console.log('ionViewDidLoad KycKnowledgePage');
-		setTimeout(() => {
-		    loading.dismiss();
-		}, 1000);
+		this.knowledge.controls['f2_proficiency'].valueChanges.subscribe(() => {
+			this.knowledge.get('f2_points').setValue(this.getTotal(), { emitEvent: false }); 
+			console.log(this.knowledge.get('f2_points').value);
+		});
+		this.knowledge.controls['f2_participation'].valueChanges.subscribe(() => {
+			this.knowledge.get('f2_points').setValue(this.getTotal(), { emitEvent: false }); 
+			console.log(this.knowledge.get('f2_points').value);
+		});
+	}
+
+	setValidation(){
+		let controls = this.knowledge.controls;
+		if(controls['f2_participation'].value == 'yes'){
+			controls['f2_typeprog'].enable();
+			controls['f2_durprog' ].enable();
+			controls['f2_condprog'].enable();
+			controls['f2_cropprog'].enable();
+		}
+		else{
+			controls['f2_typeprog'].disable();
+			controls['f2_durprog' ].disable();
+			controls['f2_condprog'].disable();
+			controls['f2_cropprog'].disable();
+		}
+	}
+
+	getTotal(){
+
+		let values = this.knowledge.getRawValue();
+		let points = {};
+		let total:number = 0;
+		points['f2_edudetail']     = 0;
+		points['f2_proficiency']   = 0;
+		points['f2_participation'] = 0;
+
+		//setting points based on values
+		//f2_edudetail
+		switch (values['f2_edudetail']) {
+			case "illiterate":
+				points['f2_edudetail'] = 2;
+				break;
+			case "primary education":
+				points['f2_edudetail'] = 4;
+				break;
+			case "matriculate":
+				points['f2_edudetail'] = 6;
+				break;
+			case "graduate":
+				points['f2_edudetail'] = 8;
+				break;
+			case "post graduate":
+				points['f2_edudetail'] = 10;
+				break;
+		}
+
+		//f2_proficiency
+		switch (values['f2_proficiency']) {
+			case "read write":
+				points['f2_proficiency'] = 10;
+				break;
+			case "read only":
+				points['f2_proficiency'] = 5;
+				break;
+			case "understand only":
+				points['f2_proficiency'] = 0;
+				break;
+		}
+
+		//f2_participation
+		switch (values['f2_participation']) {
+			case "yes":
+				points['f2_participation'] = 10;
+				break;
+			case "no":
+				points['f2_participation'] = 0;
+				break;
+		}
+
+		//sum of calculated points
+		for(let point in points){
+			total += Number(points[point]);
+		}
+
+		return total;
+		// this.knowledge.get('f2_points').setValue(total, { emitEvent: false });
+		// console.log(total);
 	}
 
 	save(){
+		this.submitAttempt = true;
 		if (this.knowledge.valid) {
 			console.log(this.knowledge.value);
 		}else{
-			console.log('Validation error');
+			console.log('Validation error', this.knowledge.controls);
 		}
 	}
 }
--- src/pages/kyc-phone/kyc-phone.ts
+++ src/pages/kyc-phone/kyc-phone.ts
@@ -18,7 +18,7 @@ export class KycPhonePage {
     phone: FormGroup;
 	constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
 		this.phone = formBuilder.group({
-			'f5_points' : [''],
+			'f5_points' : ['0'],
 
 			'f5_phonetype' : ['', Validators.required],
 			'f5_any_one_have_smart_phone' : ['', Validators.required],
@@ -35,6 +35,128 @@ export class KycPhonePage {
 
 	ionViewDidLoad() {
 		console.log('ionViewDidLoad KycPhonePage');
+
+		//update validation here
+		this.setValidation();
+
+		//Listen for form changes
+		this.phone.controls['f5_phonetype'].valueChanges.subscribe(() => {
+			this.setValidation();
+			this.getTotal()
+		});
+
+		this.phone.controls['f5_any_one_have_smart_phone'].valueChanges.subscribe(() => {
+			this.getTotal()
+		});
+
+		this.phone.controls['f5_datapack'].valueChanges.subscribe(() => {
+			this.setValidation();
+			this.getTotal()
+		});
+
+		this.phone.controls['f5_farmapp'].valueChanges.subscribe(() => {
+			this.getTotal()
+		});
+
+		this.phone.controls['f5_appuse'].valueChanges.subscribe(() => {
+			this.setValidation();
+		});
+	}
+
+	setValidation(){
+		let controls = this.phone.controls;
+		if(controls['f5_phonetype'].value === 'smartphone'){
+
+			controls['f5_datapack'].enable({ emitEvent: false });
+			controls['f5_appuse'].enable({ emitEvent: false });
+			controls['f5_app_name'].enable({ emitEvent: false });
+			controls['f5_farmapp'].enable({ emitEvent: false });
+		}
+		else{
+			controls['f5_datapack'].setValue('', { emitEvent: false });
+			controls['f5_appuse'].setValue('', { emitEvent: false });
+			controls['f5_app_name'].setValue('', { emitEvent: false });
+			controls['f5_farmapp'].setValue('', { emitEvent: false });
+
+			controls['f5_datapack'].disable({ emitEvent: false });
+			controls['f5_appuse'].disable({ emitEvent: false });
+			controls['f5_app_name'].disable({ emitEvent: false });
+			controls['f5_farmapp'].disable({ emitEvent: false });
+		}
+
+		if(controls['f5_appuse'].value === 'yes'){
+			controls['f5_app_name'].enable({ emitEvent: false });
+		}
+		else{
+			controls['f5_app_name'].setValue('', { emitEvent: false });
+			controls['f5_app_name'].disable({ emitEvent: false });
+		}
+	}
+
+	getTotal(){
+		let values = this.phone.getRawValue();
+		let points = {};
+		let total:number = 0;
+		points['f5_phonetype']                = 0;
+		points['f5_any_one_have_smart_phone'] = 0;
+		points['f5_datapack']                 = 0;
+		points['f5_farmapp']                  = 0;
+
+		//setting points based on values
+		//f5_phonetype
+		switch (values['f5_phonetype']) {
+			case "smartphone":
+				points['f5_phonetype'] = 10;
+				break;
+			case "featurephone":
+				points['f5_phonetype'] = 5;
+				break;
+		}
+
+		//f5_any_one_have_smart_phone
+		switch (values['f5_any_one_have_smart_phone']) {
+			case "yes":
+				points['f5_any_one_have_smart_phone'] = 10;
+				break;
+			case "no":
+				points['f5_any_one_have_smart_phone'] = 0;
+				break;
+		}
+
+		//f5_datapack
+		switch (values['f5_datapack']) {
+			case "yes":
+				points['f5_datapack'] = 10;
+				break;
+			case "no":
+				points['f5_datapack'] = 0;
+				break;
+		}
+
+		//f5_farmapp
+		switch (values['f5_farmapp']) {
+			case "yes":
+				points['f5_farmapp'] = 10;
+				break;
+			case "no":
+				points['f5_farmapp'] = 0;
+				break;
+		}
+
+		
+		//sum of calculated points
+		for(let point in points){
+			total += Number(points[point]);
+		}
+
+		console.log(total);
+		if(values['f5_phonetype'] == 'smartphone'){
+			total = total/4;
+		}else{
+			total = total/2;
+		}
+
+		this.phone.get('f5_points').setValue(total, { emitEvent: false });
 	}
 
 	save(){
