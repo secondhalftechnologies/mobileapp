ac1c0f0b7b854b643714ce790d8c205cf1aab3f3
Ejaz Ansari (Ejaz47) <ejjuak47@gmail.com>
2017-10-17 00:46:45 +0530

    point calc asset-details and current-crop

--- src/pages/assets-details/assets-details.ts
+++ src/pages/assets-details/assets-details.ts
@@ -19,20 +19,180 @@ export class AssetsDetailsPage {
 	assets: FormGroup;
 	constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
 		this.assets = formBuilder.group({
-			'f12_points' : [''],
+			'f12_points' : ['0'],
 
 			'f12_vehicle' : ['', Validators.required],
-			'f12_total_val_of_vehical' : [''],
-			'f12_machinery' : [''],
-			'f12_total_val_of_machinery' : [''],
+			'f12_total_val_of_vehical' : ['', Validators.compose([ Validators.required, Validators.maxLength(10), Validators.pattern('^[+-]?([0-9]*[.])?[0-9]+$')]) ],
+			'f12_machinery' : ['', Validators.required],
+			'f12_total_val_of_machinery' : ['', Validators.compose([ Validators.required, Validators.maxLength(10), Validators.pattern('^[+-]?([0-9]*[.])?[0-9]+$')]) ],
 			'f12_any_other_assets' : [''],
 			'f12_name_of_other_assets' : [''],
-			'f12_mention_value_of_assets' : [''],
+			'f12_mention_value_of_assets' : ['', Validators.compose([ Validators.required, Validators.maxLength(10), Validators.pattern('^[+-]?([0-9]*[.])?[0-9]+$')]) ],
 		});
+	}
+
+	setValidation(){
+		let controls = this.assets.controls;
+		if(controls['f12_any_other_assets'].value == 'yes'){
+			controls['f12_name_of_other_assets'].enable();
+			controls['f12_mention_value_of_assets'].enable();
+		}
+		else{
+			controls['f12_name_of_other_assets'].setValue('', { emitEvent: false });
+			controls['f12_mention_value_of_assets'].setValue('', { emitEvent: false });
+			controls['f12_name_of_other_assets'].disable();
+			controls['f12_mention_value_of_assets'].disable();
+		}
 	}
 
 	ionViewDidLoad() {
 		console.log('ionViewDidLoad AssetsDetailsPage');
+
+		this.setValidation();
+		//Listen for form changes
+		this.assets.controls['f12_any_other_assets'].valueChanges.subscribe(() => {
+			this.setValidation();
+			this.getTotal();
+		});
+
+		this.assets.controls['f12_vehicle'].valueChanges.subscribe(() => {
+			this.getTotal();
+		});
+
+		this.assets.controls['f12_total_val_of_vehical'].valueChanges.subscribe(() => {
+			this.getTotal();
+		});
+
+		this.assets.controls['f12_machinery'].valueChanges.subscribe(() => {
+			this.getTotal();
+		});
+
+		this.assets.controls['f12_total_val_of_machinery'].valueChanges.subscribe(() => {
+			this.getTotal();
+		});
+
+		this.assets.controls['f12_mention_value_of_assets'].valueChanges.subscribe(() => {
+			this.getTotal();
+		});
+
+	}
+
+	getTotal(){
+		let values = this.assets.getRawValue();
+		let points = {};
+		let total:number = 0;
+		points['f12_vehicle']                 = 0;
+		points['f12_total_val_of_vehical']    = 0;
+		points['f12_machinery']               = 0;
+		points['f12_total_val_of_machinery']  = 0;
+		points['f12_mention_value_of_assets'] = 0;
+
+
+		//setting points based on values
+		//f12_vehicle
+		switch (values['f12_vehicle']) {
+			case "1":
+				points['f12_vehicle'] = 5;
+				break;
+			case "2":
+				points['f12_vehicle'] = 7;
+				break;
+			case "3":
+				points['f12_vehicle'] = 8;
+				break;
+			case "4_or_more":
+				points['f12_vehicle'] = 10;
+				break;
+		}
+
+		//f12_total_val_of_vehical
+		switch (values['f12_total_val_of_vehical']) {
+			case values['f12_total_val_of_vehical'] >= 0 && values['f12_total_val_of_vehical'] <= 50000:
+				points['f12_total_val_of_vehical'] = 2;
+				break;
+			case values['f12_total_val_of_vehical'] >= 50001 && values['f12_total_val_of_vehical'] <= 100000:
+				points['f12_total_val_of_vehical'] = 4;
+				break;
+			case values['f12_total_val_of_vehical'] >= 100001 && values['f12_total_val_of_vehical'] <= 500000:
+				points['f12_total_val_of_vehical'] = 6;
+				break;
+			case values['f12_total_val_of_vehical'] >= 500001 && values['f12_total_val_of_vehical'] <= 1000000:
+				points['f12_total_val_of_vehical'] = 8;
+				break;
+			case values['f12_total_val_of_vehical'] > 1000000:
+				points['f12_total_val_of_vehical'] = 10;
+				break;
+		}
+
+		//f12_machinery
+		switch (values['f12_machinery']) {
+			case "1":
+				points['f12_machinery'] = 2;
+				break;
+			case "2":
+				points['f12_machinery'] = 4;
+				break;
+			case "3":
+				points['f12_machinery'] = 6;
+				break;
+			case "4_or_more":
+				points['f12_machinery'] = 0;
+				break;
+		}
+
+		//f12_total_val_of_machinery
+		switch (values['f12_total_val_of_machinery']) {
+			case values['f12_total_val_of_machinery'] >= 0 && values['f12_total_val_of_machinery'] <= 50000:
+				points['f12_total_val_of_machinery'] = 2;
+				break;
+			case values['f12_total_val_of_machinery'] >= 50001 && values['f12_total_val_of_machinery'] <= 100000:
+				points['f12_total_val_of_machinery'] = 4;
+				break;
+			case values['f12_total_val_of_machinery'] >= 100001 && values['f12_total_val_of_machinery'] <= 500000:
+				points['f12_total_val_of_machinery'] = 6;
+				break;
+			case values['f12_total_val_of_machinery'] >= 500001 && values['f12_total_val_of_machinery'] <= 1000000:
+				points['f12_total_val_of_machinery'] = 8;
+				break;
+			case values['f12_total_val_of_machinery'] > 1000000:
+				points['f12_total_val_of_machinery'] = 10;
+				break;
+		}
+
+
+		//f12_mention_value_of_assets
+		switch (values['f12_mention_value_of_assets']) {
+			case values['f12_mention_value_of_assets'] >= 0 && values['f12_mention_value_of_assets'] <= 50000:
+				points['f12_mention_value_of_assets'] = 2;
+				break;
+			case values['f12_mention_value_of_assets'] >= 50001 && values['f12_mention_value_of_assets'] <= 100000:
+				points['f12_mention_value_of_assets'] = 4;
+				break;
+			case values['f12_mention_value_of_assets'] >= 100001 && values['f12_mention_value_of_assets'] <= 500000:
+				points['f12_mention_value_of_assets'] = 6;
+				break;
+			case values['f12_mention_value_of_assets'] >= 500001 && values['f12_mention_value_of_assets'] <= 1000000:
+				points['f12_mention_value_of_assets'] = 8;
+				break;
+			case values['f12_mention_value_of_assets'] > 1000000:
+				points['f12_mention_value_of_assets'] = 10;
+				break;
+		}
+
+		//sum of calculated points
+		for(let point in points){
+			total += Number(points[point]);
+		}
+
+		console.log(total);
+		if(values['f12_any_other_assets'] == 'yes'){
+			total = parseFloat((total/5).toFixed(2));
+		}
+		else{
+			total = parseFloat((total/4).toFixed(2));
+		}
+
+		this.assets.get('f12_points').setValue(total, { emitEvent: false });
 	}
 
 	save(){
--- src/pages/crop-current-add/crop-current-add.ts
+++ src/pages/crop-current-add/crop-current-add.ts
@@ -16,35 +16,181 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CropCurrentAddPage {
 	current: FormGroup;
+  	submitAttempt: boolean = false;
+
 	constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
 		this.current = formBuilder.group({
-			'f14_points' : [''],
+			'f14_points' : ['0'],
 			
-			'f14_crop_type' : [''],
-			'f14_cultivating' : [''],
-			'f14_variety' : [''],
-			'f14_total_acrage' : [''],
-			'f14_expected_yeild' : [''],
-			'f14_seed_type' : [''],
-			'f14_seed_quantity' : [''],
-			'f14_spend_money' : [''],
-			'f14_use_self_grown_seeds' : [''],
-			'f14_loan_taken' : [''],
-			'f14_loan_amount' : [''],
-			'f14_borrow_loan_from' : [''],
-			'f14_diseases' : [''],
-			'f14_water_source_type' : [''],
-			'f14_harvest_date' : [''],
-			'f14_income' : [''],
+			'f14_crop_type' : ['', Validators.required ],
+			'f14_cultivating' : ['', Validators.required ],
+			'f14_variety' : ['', Validators.required ],
+			'f14_total_acrage' : ['', Validators.compose([ Validators.required, Validators.maxLength(10), Validators.pattern('^[+-]?([0-9]*[.])?[0-9]+$')]) ],
+			'f14_expected_yeild' : ['', Validators.compose([ Validators.required, Validators.maxLength(10), Validators.pattern('^[+-]?([0-9]*[.])?[0-9]+$')]) ],
+			'f14_seed_type' : ['', Validators.required ],
+			'f14_seed_quantity' : ['', Validators.compose([ Validators.required, Validators.maxLength(10), Validators.pattern('^[+-]?([0-9]*[.])?[0-9]+$')]) ],
+			'f14_spend_money' : ['', Validators.compose([ Validators.required, Validators.maxLength(10), Validators.pattern('^[+-]?([0-9]*[.])?[0-9]+$')]) ],
+			'f14_use_self_grown_seeds' : ['', Validators.required ],
+			'f14_loan_taken' : ['', Validators.required ],
+			'f14_loan_amount' : ['', Validators.compose([ Validators.required, Validators.maxLength(10), Validators.pattern('^[+-]?([0-9]*[.])?[0-9]+$')]) ],
+			'f14_borrow_loan_from' : ['', Validators.required ],
+			'f14_diseases' : ['', Validators.required ],
+			'f14_water_source_type' : ['', Validators.required ],
+			'f14_harvest_date' : ['', Validators.required ],
+			'f14_income' : ['', Validators.required ],
 			
 		});
 	}
 
+	setValidation(){
+		let controls = this.current.controls;
+		if(controls['f14_loan_taken'].value == 'yes'){
+			controls['f14_loan_amount'].enable();
+			controls['f14_borrow_loan_from'].enable();
+		}
+		else{
+			controls['f14_loan_amount'].setValue('', { emitEvent: false });
+			controls['f14_borrow_loan_from'].setValue('', { emitEvent: false });
+			controls['f14_loan_amount'].disable();
+			controls['f14_borrow_loan_from'].disable();
+		}
+	}
+
+
 	ionViewDidLoad() {
 		console.log('ionViewDidLoad LandFarmAddPage');
+
+		this.setValidation();
+
+		//Listen for form changes
+		this.current.controls['f14_seed_type'].valueChanges.subscribe(() => {
+			this.getTotal();
+		});
+
+		this.current.controls['f14_loan_taken'].valueChanges.subscribe(() => {
+			this.setValidation();
+			this.getTotal();
+		});
+
+		this.current.controls['f14_loan_amount'].valueChanges.subscribe(() => {
+			this.getTotal();
+		});
+
+		this.current.controls['f14_borrow_loan_from'].valueChanges.subscribe(() => {
+			this.getTotal();
+		});
+
+		this.current.controls['f14_water_source_type'].valueChanges.subscribe(() => {
+			this.getTotal();
+		});
+
+	}
+
+	getTotal(){
+		let values = this.current.getRawValue();
+		let points = {};
+		let total:number = 0;
+		points['f14_seed_type']         = 0;
+		points['f14_loan_taken']        = 0;
+		points['f14_loan_amount']       = 0;
+		points['f14_borrow_loan_from']  = 0;
+		points['f14_water_source_type'] = 0;
+
+
+		//setting points based on values
+		//f14_seed_type
+		switch (values['f14_seed_type']) {
+			case "Hybrid":
+				points['f14_seed_type'] = 10;
+				break;
+			case "Non-hybrid":
+				points['f14_seed_type'] = 0;
+				break;
+		}
+
+		//f14_loan_taken
+		switch (values['f14_loan_taken']) {
+			case "yes":
+				points['f14_loan_taken'] = 0;
+				break;
+			case "no":
+				points['f14_loan_taken'] = 10;
+				break;
+		}
+
+		//f14_loan_amount
+		switch (values['f14_loan_amount']) {
+			case values['f14_loan_amount'] >= 0 && values['f14_loan_amount'] <= 5000:
+				points['f14_loan_amount'] = 10;
+				break;
+			case values['f14_loan_amount'] >= 5001 && values['f14_loan_amount'] <= 15000:
+				points['f14_loan_amount'] = 8;
+				break;
+			case values['f14_loan_amount'] >= 15001 && values['f14_loan_amount'] <= 30000:
+				points['f14_loan_amount'] = 6;
+				break;
+			case values['f14_loan_amount'] >= 30001 && values['f14_loan_amount'] <= 45000:
+				points['f14_loan_amount'] = 4;
+				break;
+			case values['f14_loan_amount'] > 45000:
+				points['f14_loan_amount'] = 2;
+				break;
+		}
+		
+		//f14_borrow_loan_from
+		switch (values['f14_borrow_loan_from']) {
+			case "Cooprative Bank":
+				points['f14_borrow_loan_from'] = 0;
+				break;
+			case "Bank":
+				points['f14_borrow_loan_from'] = 10;
+				break;
+			case "MFI OR NBFC":
+				points['f14_borrow_loan_from'] = 8;
+				break;
+			case "FPO":
+				points['f14_borrow_loan_from'] = 6;
+				break;
+			case "Money Lender":
+				points['f14_borrow_loan_from'] = 0;
+				break;
+			case "Other Lending Institutions":
+				points['f14_borrow_loan_from'] = 2;
+				break;
+		}
+
+		//f14_water_source_type
+		switch (values['f14_water_source_type']) {
+			case "Rainwater Only":
+				points['f14_water_source_type'] = 2;
+				break;
+			case "Irrigation":
+				points['f14_water_source_type'] = 8;
+				break;
+			case "Canals":
+				points['f14_water_source_type'] = 4;
+				break;
+			case "Others":
+				points['f14_water_source_type'] = 6;
+				break;
+		}
+
+		//sum of calculated points
+		for(let point in points){
+			total += Number(points[point]);
+		}
+
+		console.log(total);
+		if(values['f14_loan_taken'] == 'yes'){
+			total = parseFloat((total/5).toFixed(2));
+		}else{
+			total = parseFloat((total/3).toFixed(2));
+		}
+		this.current.get('f14_points').setValue(total, { emitEvent: false });
 	}
 
 	save(){
+		this.submitAttempt = true;
 		if (this.current.valid) {
 			console.log(this.current.value);
 		}else{
